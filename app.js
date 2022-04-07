6e1f24c250f7e040b7373ca0f625bd20a40394ed
Garcia <egarcia4@FVFY36RHJ1WK.tld>
2022-04-07 12:21:23 -0500

    done with week12

---
+++ app.js
@@ -0,0 +1,71 @@
+let userNum = document.getElementById("getNumber");
+userNum.addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) {
+        run(e);
+    }
+
+});
+
+userNum.focus();
+
+let counter = 0;
+
+
+function run() {
+    randomNum();
+    userNumber();
+    compareNumbers();
+
+}
+
+function randomNum() {
+    let ran = Math.floor(Math.random()* 10);
+    return ran;
+}
+
+function userNumber() {
+    let user = document.getElementById("getNumber").value;
+    let y = document.getElementById("userNumber")
+    return user;
+}
+
+function compareNumbers() {
+    let a = userNumber();
+    let b = randomNum ();
+    let z = document.getElementById("compare");
+    let c = document.getElementById("counter");
+
+    if (a != b) {
+        z.innerHTML = "Numbers are not the same. Computer got " + b + ", and the user got  " + a;
+        z.style.color = "white";
+        z.style.backgroundColor = "red";
+        z.style.padding = "20px";
+        z.style.textAlign = "center";
+        counter++
+        c.innerHTML = "You have tried " + counter + " time(s)."
+        c.style.color = "white";
+        c.style.backgroundColor = "grey";
+        c.style.padding = "20px";
+        c.style.textAlign = "center";
+        document.querySelector("body").style.backgroundColor = "pink"
+    } else if (a=b) {
+        z.innerHTML = "Numbers are the same. Computer got " + b + ", and the user got  " +  a ;
+        z.style.color = "white";
+        z.style.backgroundColor = "green";
+        z.style.padding = "20px";
+        z.style.textAlign = "center";
+        c.innerHTML = "You have tried " + counter + " time(s) to get it right."
+        c.style.color = "white";
+        c.style.backgroundColor = "grey";
+        c.style.padding = "20px";
+        c.style.textAlign = "center";
+        counter = 0;
+        document.querySelector("body").style.backgroundColor = "lime"
+    }
+
+    resetInput();
+}
+
+function resetInput() {
+   let b = document.getElementById("getNumber").value ="";
+}
